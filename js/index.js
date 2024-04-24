8f0223b4f971a8555dca746e2eb204e43918b44e
Oleg Sysolyatin <osysolyatin@gmail.com>
2024-04-24 14:39:41 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// Smooth scroll
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const sectionID = this.getAttribute("href").slice(1);
+
+    document.getElementById(sectionID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+// Slider for City-About section
+const swiper = new Swiper(".city-about-swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  // direction: "vertical",
+  // loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".city-about-swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".city-about-swiper-button-next",
+    prevEl: ".city-about-swiper-button-prev",
+  },
+});
